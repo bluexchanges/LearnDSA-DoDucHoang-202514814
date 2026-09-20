a8b25624f9005b0e6557ade5761f7f0a2f89dcf7
Alanwhte <hoangsongtu2405@gmail.com>
2026-09-20 23:46:30 +0700

    Sua bubble sort

--- my_sort/Bubble_sort.hpp
+++ my_sort/Bubble_sort.hpp
@@ -4,15 +4,19 @@
 #include<algorithm> 
 template <typename T>
 void bubble_sort(std::vector<T> &arr){
+    bool swapped;
     for(int i=0; i< arr.size();i++){
+        swapped = false;
         for(int j=0;j<arr.size()-i-1;j++){
         if(arr[j]>arr[j+1]){
             T temp;
             temp = arr[j];
             arr[j]=arr[j+1];
             arr[j+1]=temp;
+            swapped= true;
         }
         }
+        if (swapped == false) break;
     }
 }
 #endif
