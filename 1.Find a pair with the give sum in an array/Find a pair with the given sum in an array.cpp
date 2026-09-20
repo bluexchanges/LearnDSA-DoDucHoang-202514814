a1672023b8bb2637aa6bd6ebe228ddebb583f395
Alanwhte <hoangsongtu2405@gmail.com>
2026-09-21 00:32:49 +0700

    Find a pair

---
+++ 1.Find a pair with the give sum in an array/Find a pair with the given sum in an array.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include "../my_sort/Bubble_sort.hpp"
+using namespace std;
+void Find_pair(vector<int>&arr,int S ){
+    if(arr.size()<2){
+        cout<<"Khong tim thay";
+        return;
+    }
+    bubble_sort(arr);
+    int low =0;
+    int high= arr.size()-1;
+    while(low<high){
+      long long sum = 1LL*arr[low]+arr[high];
+      if(sum==S){
+        cout<< "Pair found(" << arr[low]<< "," << arr[high] << ")\n";
+        return;
+      }
+      (sum<S) ? low++: high--;
+    }
+   cout<<"Khong tim thay\n";
+}
+int main(){
+    int n,S;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cin>>S;
+    Find_pair(arr,S);
+}
